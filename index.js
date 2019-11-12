10b31f8d902a8c48cfa7dbad0afe18150c6c35d6
Joshua Saucedo <joshuansu0897@gmail.com>
2019-11-12 13:22:49 -0700

    literalmente solo falta hacer la operacion aritmetica

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.on('ready', () => {
   let window = new BrowserWindow({
     width: 800,
     height: 600,
-    title: 'Hola Mundo!',
+    title: 'Modelo P',
     center: true,
     maximizable: false,
     show: false
@@ -28,3 +28,8 @@ app.on('ready', () => {
 
   window.loadURL(`file://${__dirname}/src/renderer/components/explicacion/index.html`)
 })
+
+exports.handleForm = function handleForm(targetWindow, firstname) {
+  console.log("this is the firstname from the form ->", firstname)
+  targetWindow.webContents.send('form-received', "we got it")
+}
--- src/renderer/components/calculadora/logic.js
+++ src/renderer/components/calculadora/logic.js
@@ -1,3 +1,9 @@
-window.addEventListener('load', () => {
+'use strict'
+const submitFormButton = document.getElementById('form')
+const responseText = document.getElementById('response')
+submitFormButton.addEventListener('submit', (event) => {
+  event.preventDefault()
+  let firstname = document.getElementById('firstname').value
 
+  responseText.innerHTML = firstname
 })
